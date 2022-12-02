eb64e694734905b63f4794dfd03432f1b0457c3d
Yaroslava <119040485+YaroslavaMalaya@users.noreply.github.com>
2022-12-02 11:02:33 +0200

    main
    transformation numbers

---
+++ main.py
@@ -0,0 +1,37 @@
+def conversion_to_binary(number):              # переведення в двійкове
+    remainder = int(number) % 2                # остача
+    integer = int(number) // 2                 # ціла частина
+    x = [remainder]
+    while integer != 0:
+        remainder = integer % 2
+        integer //= 2
+        x.insert(0, remainder)                   # зберігти елемнт на початку списка
+    print(f'This is 0b{"".join(map(str, x))}')  # об'єдную усі елементи як стрінг в списку і прінчу
+
+
+def conversion_to_decimal(number):               # переведення в десяткове
+    decimal_number = 0
+    binary_number = number.split('b')[-1]
+    len_number = len(str(binary_number))
+    for i in range(0, len_number):
+        decimal_number += int(binary_number[i]) * (2 ** (int(len_number) - i - 1))
+    print(f'This is {decimal_number}')
+
+
+def transformation():
+    number = input('Enter your number: ')
+    if number[:2] == '0b':
+        conversion_to_decimal(number)
+    else:
+        conversion_to_binary(number)
+
+
+transformation()
+answer = input('Do you wanna continue? ')
+while True:
+    if answer.lower() == 'yes':
+        transformation()
+        answer = input('Do you wanna continue? ')
+    elif answer.lower() == 'no':
+        print('Ok, goodbye.')
+        break
